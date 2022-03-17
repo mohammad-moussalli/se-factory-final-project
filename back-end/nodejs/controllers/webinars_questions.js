81c862720519a35f337960ab012d9c00e321366e
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:39:46 +0200

    Implement Controllers for Webinar_Question Model

---
+++ back-end/nodejs/controllers/webinars_questions.js
@@ -0,0 +1,46 @@
+const webinarQuestionServices = require ("../services/webinar_questions");
+
+module.exports = {
+    getQuestion,
+    getQuestions,
+    createQuestions,
+    deleteQuestions,
+    update,
+    delete: _delete,
+}
+
+function createQuestions(req, res, next) {
+    webinarQuestionServices.createQuestions(req.body)
+        .then((message) => res.json({message : message}))
+        .catch(next);
+}
+
+function getQuestion(req, res, next) {
+    webinarQuestionServices.getQuestion(req.params.id)
+        .then((response) => res.json(response))
+        .catch(next);
+}
+
+function getQuestions(req, res, next) {
+    webinarQuestionServices.getQuestions(req.body.country)
+        .then((response) => res.json(response))
+        .catch(next);
+}
+
+function update(req, res, next) {
+    webinarQuestionServices.update(req.body)
+    .then((message) => res.json({message : message}))
+    .catch(next);
+}
+
+function _delete(req, res, next) {
+    webinarQuestionServices.delete(req.params.id)
+        .then((message) => res.json({ message: message }))
+        .catch(next);
+}
+
+function deleteQuestions(req, res, next) {
+    webinarQuestionServices.deleteQuestions(req.body)
+        .then((message) => res.json({ message: message }))
+        .catch(next);
+}

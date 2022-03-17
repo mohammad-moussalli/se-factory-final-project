872f6acd416b23ac6021d7cae38d844c94d5bc4e
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 12:30:54 +0200

    Implement Webinar_Question Model

---
+++ back-end/nodejs/models/webinar_questions.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Webinar_Questions extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Webinar_Questions.init({
+    webinar_id: DataTypes.INTEGER,
+    question: DataTypes.STRING,
+    answer: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Webinar_Questions',
+  });
+  return Webinar_Questions;
+};

b8600cfda9e1cadfef302607cc76b14242a33735
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 12:38:39 +0200

    Implement university migration

---
+++ back-end/nodejs/migrations/20220317103634-create-university.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Universities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      university: {
+        type: Sequelize.STRING
+      },
+      major: {
+        type: Sequelize.STRING
+      },
+      degree: {
+        type: Sequelize.STRING
+      },
+      university_country: {
+        type: Sequelize.STRING
+      },
+      university_city: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Universities');
+  }
+};

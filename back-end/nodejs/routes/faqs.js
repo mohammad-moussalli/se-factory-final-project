5e90d2452eec6f9eb63445abcd15e87a9e28910e
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:16:57 +0200

    Implement Faq Api Routes

---
+++ back-end/nodejs/routes/faqs.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var faq = require("../controllers/faqs")
+
+/* GET users listing. */
+router.get('/category/:category_id', faq.getFaqs);
+router.post('/add', faq.create);
+router.get('/:id', faq.getFaq)
+router.post('/update', faq.update)
+router.delete('/:id', faq.delete);
+router.delete('/category/:category_id', faq.deleteCategoryFaqs);
+
+module.exports = router;

df03e5f837f32ce8a3f648e4e4075e84bf4d1788
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:42:38 +0200

    Implement Controllers for Webinar Model

---
+++ back-end/nodejs/controllers/webinars.js
@@ -0,0 +1,39 @@
+const webinarServices = require ("../services/webinars");
+
+module.exports = {
+    create,
+    getWebinar,
+    getWebinarByCountry,
+    update,
+    delete: _delete,
+}
+
+function create(req, res, next) {
+    webinarServices.create(req.body)
+        .then((message) => res.json({message : message}))
+        .catch(next);
+}
+
+function getWebinar(req, res, next) {
+    webinarServices.getWebinar(req.params.id)
+        .then((response) => res.json(response))
+        .catch(next);
+}
+
+function getWebinarByCountry(req, res, next) {
+    webinarServices.getWebinarByCountry(req.body)
+        .then((response) => res.json(response))
+        .catch(next);
+}
+
+function update(req, res, next) {
+    webinarServices.update(req.body)
+    .then((message) => res.json({message : message}))
+    .catch(next);
+}
+
+function _delete(req, res, next) {
+    webinarServices.delete(req.params.id)
+        .then((message) => res.json({ message: message }))
+        .catch(next);
+}

621925e5064e86c2351d129eac51f3b64e2d0546
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:38:23 +0200

    Implement CRUD functions for Webinar_Question Model

---
+++ back-end/nodejs/services/webinars_questions.js
@@ -0,0 +1,96 @@
+const model = require('../models')
+
+module.exports = {
+    getQuestion,
+    getQuestions,
+    createQuestions,
+    deleteQuestions,
+    update,
+    delete: _delete,
+}
+
+async function getQuestions(country) {
+    
+    const webinar = await model.Webinar.max('id', {where:{country: country}})
+    if (!webinar){
+        return {message: 'Webinar not found'};
+    }
+
+    const webinar_questions = await model.Webinar_Questions.findAll({where:{webinar_id: webinar}})
+    if(!webinar_questions){
+        return "Webinar questions not found";
+    }
+    return webinar_questions;
+}
+
+async function getQuestion(id) {
+    const webinar_question = await model.Webinar_Questions.findByPk(id);
+    if (!webinar_question){
+        return {message: 'Webinar question not found'};
+    }
+    return webinar_question;
+}
+
+async function createQuestions(params) {
+    // validate
+    const webinar = await model.Webinar.findByPk(params.webinar_id)
+    if (!webinar){
+        return "Webinar Not Found"
+    }
+    if (await model.Webinar_Questions.findOne({ where: { webinar_id: params.webinar_id, question: params.question, answer: params.answer } })) {
+        return "Webinar question already exists"
+    }
+    // save webinar
+    await model.Webinar_Questions.create({webinar_id: params.webinar_id, question: params.question, answer: params.answer });
+    return "Webinar question created successfully"
+}
+
+async function update(params) {
+    const webinar_question = await getQuestion(params.id);
+    // validate
+    console.log(webinar_question)
+    if(webinar_question.message === "Webinar question not found"){
+        return 'Webinar not found'
+    }
+    if (!params.country){
+        params.country = webinar_question.country
+    }
+    if (!params.answer){
+        params.answer = webinar_question.answer
+    }
+    if (!params.webinar_id){
+        params.webinar_id = webinar_question.webinar_id
+    }
+    const webinarQuestionChanged = webinar_question.webinar_id !== params.webinar_id || webinar_question.question !== params.question || webinar_question.answer !== params.answer;
+    if (webinarQuestionChanged && await model.Webinar_Questions.findOne({ where: { webinar_id :params.webinar_id, question :params.question } })) {
+        return 'Webinar Question already exists';
+    }
+
+    // update params to faq and save but not working directly on postman
+    model.Webinar_Questions.update(
+        { webinar_id : params.webinar_id, question : params.question, answer : params.answer  },	
+        { where: { id: params.id } },	 
+      )
+    
+    return "Updated Successfuly";
+}
+
+async function _delete(id) {
+    const question = await getQuestion(id);
+    if (!question.id){
+        return 'Question is not found';
+    }
+    await question.destroy();
+    return 'Question deleted successfully';
+}
+
+async function deleteQuestions(params) {
+    const questions = await getQuestions(params.country);
+    if (!questions){
+        return 'Questions are not found';
+    }
+    await model.Webinar_Questions.destroy({
+        where:{webinar_id: questions.webinar_id}
+    })
+    return 'Questions deleted successfully';
+}

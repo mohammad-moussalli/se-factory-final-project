a13a06aa423e3b8b9bd6ca9bac86ae16ade16a5d
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 12:32:09 +0200

    Implement webinar_questions migration

---
+++ back-end/nodejs/migrations/20220317102934-create-webinar-questions.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Webinar_Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      webinar_id: {
+        type: Sequelize.INTEGER
+      },
+      question: {
+        type: Sequelize.STRING
+      },
+      answer: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Webinar_Questions');
+  }
+};

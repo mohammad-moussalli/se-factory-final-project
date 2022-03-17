66925cde5aa772a10632b0805499f56a097a17c7
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:32:50 +0200

    Implement API Routes for User

--- back-end/nodejs/routes/users.js
+++ back-end/nodejs/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var user = require("../controllers/users"); 
+const { update } = require('../services/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.post('/register', user.register);
+router.post('/login', user.login);
+router.get('/', user.getUser)
+router.post('/update', update)
 module.exports = router;

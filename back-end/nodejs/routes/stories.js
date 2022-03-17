7352858b8c2fac36231e47a05326d51e2896f541
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:28:33 +0200

    Implement API Routes for Story

---
+++ back-end/nodejs/routes/stories.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var story = require("../controllers/stories")
+
+/* GET users listing. */
+router.get('/', story.getAll);
+router.post('/add', story.create);
+router.post('/update', story.update);
+router.get('/:id', story.getStory);
+router.delete('/:id', story._delete);
+
+module.exports = router;

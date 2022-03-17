0117f1a652478998589e04fa702689e78d57d119
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 12:49:31 +0200

    Implement Story Model

---
+++ back-end/nodejs/models/story.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Story extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Story.init({
+    user_id: DataTypes.INTEGER,
+    story: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Story',
+  });
+  return Story;
+};

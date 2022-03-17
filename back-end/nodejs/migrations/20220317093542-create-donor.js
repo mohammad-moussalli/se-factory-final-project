c0cc2959b0739ce32d3e31dff9fd2cbef4f2bbef
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 11:39:12 +0200

    Implement donor migration

---
+++ back-end/nodejs/migrations/20220317093542-create-donor.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Donors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      mobile_phone: {
+        type: Sequelize.STRING
+      },
+      amount_donated: {
+        type: Sequelize.INTEGER
+      },
+      currency: {
+        type: Sequelize.STRING
+      },
+      nationality: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Donors');
+  }
+};

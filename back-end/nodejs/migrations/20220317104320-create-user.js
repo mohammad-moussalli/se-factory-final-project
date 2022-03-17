e5a02ac10d2473bc070870e0f6784bdbb1943d42
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 12:43:53 +0200

    Implement user migrations

---
+++ back-end/nodejs/migrations/20220317104320-create-user.js
@@ -0,0 +1,66 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_type_id: {
+        type: Sequelize.INTEGER
+      },
+      university_id: {
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      dob: {
+        type: Sequelize.DATEONLY
+      },
+      country_code: {
+        type: Sequelize.STRING
+      },
+      mobile_phone: {
+        type: Sequelize.STRING
+      },
+      sex: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      nationality: {
+        type: Sequelize.STRING
+      },
+      profile_picture: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};

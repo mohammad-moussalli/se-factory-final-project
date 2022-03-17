a882d143256850687c5d822f18197e0d8f8dbdb3
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:16:41 +0200

    Implement controllers for Faq Model

---
+++ back-end/nodejs/controllers/faqs.js
@@ -0,0 +1,44 @@
+const faqsServices = require ("../services/faqs");
+
+module.exports = {
+    create,
+    getFaq,
+    getFaqs,
+    update,
+    delete: _delete,
+    deleteCategoryFaqs
+}
+
+function create(req, res, next) {
+    faqsServices.create(req.body)
+        .then((message) => res.json({message : message}))
+        .catch(next);}
+
+function getFaq(req, res, next) {
+    faqsServices.getFaq(req.params.id)
+        .then((response) => res.json(response))
+        .catch(next);}
+
+function getFaqs(req, res, next) {
+    faqsServices.getFaqs(req.params.category_id)
+        .then((response) => res.json(response))
+        .catch(next);}
+
+function update(req, res, next) {
+    faqsServices.update(req.body)
+    .then((message) => res.json({message : message}))
+    .catch(next);
+}
+
+function _delete(req, res, next) {
+    faqsServices.delete(req.params.id)
+        .then((message) => res.json({ message: message }))
+        .catch(next);
+}
+
+/// Not working properly
+function deleteCategoryFaqs(req, res, next) {
+    faqsServices.deleteCategoryFaqs(req.params)
+        .then((message) => res.json({ message: message }))
+        .catch(next);
+}

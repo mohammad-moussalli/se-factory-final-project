9c2019525e7f764c90b06ba8eaf4d9bdaf663b18
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 12:26:44 +0200

    Implement scholarship_cycle migration

---
+++ back-end/nodejs/migrations/20220317102023-create-scholarship-cycle.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Scholarship_Cycles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      scholarship_id: {
+        type: Sequelize.INTEGER
+      },
+      cycle: {
+        type: Sequelize.STRING
+      },
+      start_date: {
+        type: Sequelize.DATEONLY
+      },
+      deadline: {
+        type: Sequelize.DATEONLY
+      },
+      results: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Scholarship_Cycles');
+  }
+};

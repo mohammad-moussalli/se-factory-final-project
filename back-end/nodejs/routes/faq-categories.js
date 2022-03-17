54afe240bab6f72d25d438e564d661f71e1f62c5
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:13:45 +0200

    Implement faq-categories Api Routes

---
+++ back-end/nodejs/routes/faq-categories.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var faq_categories = require("../controllers/faq_categories")
+
+/* GET users listing. */
+router.get('/', faq_categories.getAll);
+router.post('/add', faq_categories.create);
+router.get('/:id', faq_categories.getCategory)
+router.post('/update', faq_categories.update)
+router.delete('/:id', faq_categories.delete);
+
+module.exports = router;

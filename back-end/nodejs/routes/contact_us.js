ad0756df5504d13d757e375dc6399b43b794e906
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-17 13:08:27 +0200

    Implement contact-us api routes

---
+++ back-end/nodejs/routes/contact_us.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var contact_us = require("../controllers/contact_us")
+
+router.post('/', contact_us.create);
+
+
+module.exports = router;

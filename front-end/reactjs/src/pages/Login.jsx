4c711163f484541f94d9bce102e8f53ff690d7fe
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-19 11:37:42 +0200

    Implement Login Page

---
+++ front-end/reactjs/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import Login from "../components/Login";
+
+const LoginPage = () => {
+    return (
+        <div className="Login">
+            <Login />
+        </div>
+    );
+}  
+export default LoginPage; 

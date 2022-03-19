f95c06029f6e2a51967cc8dc7eeea091d8db8bd5
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-19 11:45:57 +0200

    Add routes for the project

--- front-end/reactjs/src/App.js
+++ front-end/reactjs/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import Register from './pages/Register';
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+  
+            <Route exact path="/register" element={ <Register /> }/>
+            <Route exact path='/login' element={ <Login /> } />
+            <Route exact path='/' element={ <Dashboard /> } />
+
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
-
 export default App;

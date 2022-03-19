60b5b6747f2e582f22b5a990cfa2395c8c44d8bf
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-19 11:37:02 +0200

    Implement Registration Page

---
+++ front-end/reactjs/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import Register from "../components/Register";
+
+const RegisterPage = () => {
+  return (
+    <div className="Register">
+      <Register />
+    </div>
+  );
+}  
+export default RegisterPage; 

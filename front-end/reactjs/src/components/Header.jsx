def1590ec74ceabdd6b4d3429b3f939cfb810cad
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-20 00:59:04 +0200

    Clean code

--- front-end/reactjs/src/components/Header.jsx
+++ front-end/reactjs/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="header">
             <i className="kaffi-logo"/>
             <div className="links">
-                <Link to='/dashboard' className="about-kaffi">About Kaffi</Link>
+                <Link to='/about-us' className="about-kaffi">About Kaffi</Link>
                 <Button className = "header-button" text="Sign Up"/>
                 <Link to='/menu' className="menu-logo"/>
             </div>

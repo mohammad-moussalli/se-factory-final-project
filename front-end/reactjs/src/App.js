1b43cfa49cd255197d407fce943f66fa31a048ff
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-20 01:18:42 +0200

    Add Volunteer page and route

--- front-end/reactjs/src/App.js
+++ front-end/reactjs/src/App.js
@@ -16,6 +16,7 @@ import Records from './pages/Records';
 import Scholarships from './pages/Scholarships';
 import Team from './pages/Team';
 import Webinars from './pages/Webinars';
+import Volunteer from './pages/Volunteer'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -25,8 +26,9 @@ function App() {
     <>
     
       <Router>
-      <Header />
+      
         <div className="App">
+        <Header />
           <Routes>
             <Route exact path='/' element={ <Home /> } />
             <Route exact path="/register" element={ <Register /> }/>
@@ -34,22 +36,25 @@ function App() {
             <Route exact path='/dashboard' element={ <Dashboard /> } />
             <Route exact path='/menu' element={ <Menu /> } />
             <Route exact path='/about-us' element={ <AboutUs /> } />
-            <Route exact path='/buddy/apply' element={ <BecomeBuddy /> } />
+            <Route exact path='/volunteer/buddy' element={ <BecomeBuddy /> } />
             <Route exact path='/blog' element={ <Blog /> } />
             <Route exact path='/scholarship/boost' element={ <BoostScholarship /> } />
             <Route exact path='/cv' element={ <CvTips /> } />
             <Route exact path='/donate' element={ <Donate /> } />
             <Route exact path='/buddy/find' element={ <FindBuddy /> } />
-            <Route exact path='/team/apply' element={ <JoinTeam /> } />
+            <Route exact path='/volunteer/team' element={ <JoinTeam /> } />
             <Route exact path='/scholarship/launch' element={ <LaunchScholarship /> } />
             <Route exact path='/records' element={ <Records /> } />
             <Route exact path='/scholarships' element={ <Scholarships /> } />
             <Route exact path='/team' element={ <Team /> } />
             <Route exact path='/webinars' element={ <Webinars /> } />
+            <Route exact path='/volunteer' element={ <Volunteer /> } />
+
 
           </Routes>
+          <Footer />
         </div>
-        <Footer />
+       
       </Router>
     </>
   );

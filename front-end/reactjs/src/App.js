b82ae2fb3ca36830d2e662549a86b5e719011311
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-19 22:21:10 +0200

    Add routes to App file

--- front-end/reactjs/src/App.js
+++ front-end/reactjs/src/App.js
@@ -2,20 +2,28 @@ import Register from './pages/Register';
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+    
       <Router>
+      <Header />
         <div className="App">
           <Routes>
-  
+            <Route exact path='/' element={ <Home /> } />
             <Route exact path="/register" element={ <Register /> }/>
             <Route exact path='/login' element={ <Login /> } />
-            <Route exact path='/' element={ <Dashboard /> } />
+            <Route exact path='/dashboard' element={ <Dashboard /> } />
+            <Route exact path='/menu' element={ <Menu /> } />
 
           </Routes>
         </div>
+        <Footer />
       </Router>
     </>
   );

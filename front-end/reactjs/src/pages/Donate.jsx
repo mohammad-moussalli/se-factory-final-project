e5c2da60e9d095e616546cd1736ede1bbfe3118c
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-20 04:44:51 +0200

    Add useState to Donate Page

--- front-end/reactjs/src/pages/Donate.jsx
+++ front-end/reactjs/src/pages/Donate.jsx
@@ -8,45 +8,53 @@ import DonationInfo from "../components/DonationInfo";
 import SuccessStories from "../components/SuccessStories";
 import Button from "../components/Button";
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import WireTransfer from "../components/WireTransfer";
 
 
 const Donate = () => {
 
-  const navigate = useNavigate();
-  const redirectToRecordsPage = () => {
-    navigate('/records');
-}
-  return (
-    <div className="donations">
-      <div className="donation-title donation-row">
-        <h1>Support students through your donations!</h1>
-        <h5>Methods of Payment</h5>
-      </div>
-      <div className="donation-row">
+    const navigate = useNavigate();
+    const redirectToRecordsPage = () => {
+        navigate('/records');
+    }
 
-        <DonationTag  className="donation-tag" text="Direct Bank Wire Transfer" image={BankTransfer}/>
-        <DonationTag className="donation-tag" image={PayPal}/>
+    const [showWireTransferTag, setShowWireTransferTag] = useState(false);
+    const onClick = () => {
+        setShowWireTransferTag(true);
+    }
 
-      </div>
+    return (
+        <div className="donations">
+            <div className="donation-title donation-row">
+                <h1>Support students through your donations!</h1>
+                <h5>Methods of Payment</h5>
+            </div>
+            <div className="donation-row">
+                <DonationTag  className="donation-tag" text="Direct Bank Wire Transfer" image={BankTransfer} onClick={onClick}/> 
+                <DonationTag className="donation-tag" image={PayPal}/>
+            </div>
 
-      <div className="donation-row">
+            <div className="donation-row">
+                <DonationTag className="donation-tag" image={GoFundMe}/>
+                <DonationTag className="donation-tag" image={Benevity}/>
+            </div>
 
-        <DonationTag className="donation-tag" image={GoFundMe}/>
-        <DonationTag className="donation-tag" image={Benevity}/>
+            <div className="donation-info doantion-row">
+                <DonationInfo />
+            </div>
 
-      </div>
+            <div className="success-stories donation row">
+                <Button text="View All Success Stories & Records" className="success-stories-button" onClick={redirectToRecordsPage}/>
+                <SuccessStories />
+            </div>
 
-      <div className="donation-info doantion-row">
-        <DonationInfo />
-      </div>
-
-      <div className="success-stories donation row">
-        <Button text="View All Success Stories & Records" className="success-stories-button" onClick={redirectToRecordsPage}/>
-        <SuccessStories />
-      </div>
-
-    </div>
-  )
+            {showWireTransferTag ? 
+                <WireTransfer />:
+                null
+            }
+        </div>
+    )
 }
 
 export default Donate

9b7d37f0ed248ad86ba754dcf22998efd2416867
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-21 16:48:26 +0200

    Implement FaqsTag component

---
+++ front-end/reactjs/src/components/FaqsTag.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
+import '../style/faqs-tag.css';
+
+const FaqsTag = () => {
+
+    const navigate = useNavigate();
+    const redirect = () => {
+        navigate('/faqs');
+    }
+
+    return (
+        <div className='faqs-tag'>
+            <div className='faqs-tag-header'>
+                <h4>Check our FAQ page for some common questions or email us at &nbsp;</h4> 
+                <a href="mailto: apply@kaffi-lb.org" target="_blank" className='faq-email-link'> apply@kaffi-lb.org</a>
+            </div>
+            <Button className='faqs-tag-button' text='FAQ Page' onClick={redirect}/>
+        </div>
+    )
+}
+
+export default FaqsTag

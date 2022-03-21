08520175a25ada707e2aacf8dcf32db4d512dbe4
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-21 19:31:35 +0200

    Implement LaunchScholarship Page

--- front-end/reactjs/src/pages/LaunchScholarship.jsx
+++ front-end/reactjs/src/pages/LaunchScholarship.jsx
@@ -3,17 +3,26 @@ import axios from 'axios';
 import Spinner from '../components/Spinner';
 import Button from '../components/Button'
 import { useNavigate } from 'react-router-dom';
+import ScholarshipTag from '../components/ScholarshipTag';
+import img1 from '../assets/images/KaffiLaunch.png';
+import '../style/launch-scholarship.css'
+import EligibiltyCriteria from '../components/EligibiltyCriteria';
+import HelpfulTips from '../components/HelpfulTips';
+import FaqsTag from '../components/FaqsTag';
 
 
 const LaunchScholarship = () => {
   
   const [cycle, setCycle] = useState(null)
   const scholarshipCycleApi = "http://localhost:8080/scholarships/get-cycle/"
-
   
   const navigate = useNavigate()
-  const redirect = () => {
+  const redirectToForms = () => {
     navigate('/scholarship/forms');
+  }
+
+  const redirectToScholarships = () => {
+    navigate('/scholarships');
   }
   useEffect( () => {
       axios.get(scholarshipCycleApi + "launch")
@@ -32,21 +41,58 @@ const LaunchScholarship = () => {
     )
   }  else {
     return(
-      <div>
+      <div className='launch-scholarship'> 
+        <div className='launch-scholarship-header'>
+          <div className='launch-scholarship-header-row1'>
+            <img src={img1} alt="logo"></img>
+            <h1>Kaffi-Launch Scolarship Program</h1>
+          </div>
+
+          <div className='launch-scholarship-header-row2'>
+            <div className='launch-scholarship-header-row2-col1'>
+              <h2>Program Highlights:</h2>
+              <h2>Up to €2,000 To Cover Tuition Fees</h2>
+            </div>
+
+            <div className='launch-scholarship-header-row2-col1'>
+              {cycle.cycle ?
+                <Button text='Apply Here' className='scholarship-apply-button' onClick={redirectToForms} />
+                :<Button disabled={true} text='Apply Soon' className='disabled-scholarship-apply-button'/>
+              }
+              <Button text='Check All Programs' className='check-scholarship-button' onClick={redirectToScholarships} />
+            </div>
+          </div>
+
+          <div className='launch-scholarship-header-row3'>
+            <ul>
+              <li>Support is given on a semester basis to support the student with tuition fees</li>
+              <li>Kaffi may choose to pay the tuition contribution directly to the student's university </li>
+              <li>The individual scholarship amounts are not pre-assigned, funds will be allocated on a case-by-case basis after the selection process (up to €2000)</li>
+              <li>Program does not guarantee continuous support beyond Spring 2022 (applications are reviewed each semester and scholarships are dependent on availability of funds). The goal is to support the students most in need.</li>
+            </ul>
+          </div>
+        </div>
+
         {cycle.cycle &&
           <div>
-            <div>{cycle.cycle}</div>
-            <div>{cycle.start_date}</div>
-            <div>{cycle.deadline}</div>
-            <div>{cycle.results}</div>
-            <Button disabled={!cycle.cycle} text='Apply Here' className='scolarship-apply-button' onClick={redirect} />
+            <ScholarshipTag 
+              name = 'launch'
+              cycle={cycle.cycle}
+              start_date={cycle.start_date}
+              deadline={cycle.deadline}
+              results={cycle.results}
+            />
           </div>
         }
 
+        <div>
+          <EligibiltyCriteria />
+          <HelpfulTips />
+          <FaqsTag />
+        </div>
       </div>  
     )
   }
-
 }
 
 export default LaunchScholarship

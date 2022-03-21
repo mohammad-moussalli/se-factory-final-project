ca1eafe0fe5c5d9912871fc98a642d22dba1b197
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-21 13:56:29 +0200

    Implement Boost-Scholarship Page

--- front-end/reactjs/src/pages/BoostScholarship.jsx
+++ front-end/reactjs/src/pages/BoostScholarship.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useEffect, useState} from 'react'
+import axios from 'axios';
+import Spinner from '../components/Spinner';
 
 const BoostScholarship = () => {
-  return (
-    <div>BoostScholarship</div>
-  )
+  
+  const [cycle, setCycle] = useState(null)
+  const scholarshipCycleApi = "http://localhost:8080/scholarships/get-cycle/"
+
+  useEffect( () => {
+      axios.get(scholarshipCycleApi + "boost")
+      .then(response => {
+        setCycle(response.data.response)})
+      .catch(err => {
+          console.log(err)
+      });
+  }, [])
+
+  if (!cycle){
+    return (
+      <div>
+        <Spinner/>
+      </div>
+    )
+  }  else {
+    return(
+      <div>
+        {cycle.cycle &&
+          <div>
+            <div>{cycle.cycle}</div>
+            <div>{cycle.start_date}</div>
+            <div>{cycle.deadline}</div>
+            <div>{cycle.results}</div>
+            <button disabled={!cycle.cycle}>apply</button>
+          </div>}
+        
+      </div>  
+    )
+  }
+
 }
 
 export default BoostScholarship

584ae424b4a398088c03b11be428fad8ea2e7405
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-22 18:32:56 +0200

    Add redirect to HomePage on Click

--- front-end/reactjs/src/components/Header.jsx
+++ front-end/reactjs/src/components/Header.jsx
@@ -1,9 +1,7 @@
 import "../style/header.css";
 import { Link } from "react-router-dom";
 import Button from "./Button";
-import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
-
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
@@ -27,11 +25,16 @@ const Header = () => {
         menu_logo = 'home-menu-logo';
     }
 
+    const navigate = useNavigate()
+    const redirect= () => {
+    navigate('/');
+  }
+
     return (
         
         <>
         <div className={`navbar ${navbar}`}>
-            <i className={`kaffi-logo ${kaffi_logo}`}/>
+            <i className={`kaffi-logo ${kaffi_logo}`} onClick={redirect}/>
             <div className={`links ${links}`}>
                 <Link to='/about-us' className={`about-kaffi ${about_kaffi}`}>About Kaffi</Link>
                 <Button className ={`navbar-button ${navbar_button}`} text="Sign Up"/>

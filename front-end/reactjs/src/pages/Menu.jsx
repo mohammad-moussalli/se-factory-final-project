a4d5e00f524cba3f88fd84fd79cbddd8f81a668a
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-25 22:59:11 +0200

    Add classNames for styling purposes

--- front-end/reactjs/src/pages/Menu.jsx
+++ front-end/reactjs/src/pages/Menu.jsx
@@ -68,62 +68,47 @@ const MenuPage = () => {
         </div>
 
         <div className ="menu-header">
-            <div className="menu-option">
-                <p onClick={onClick1} className='menu-option-title'>The Foundation</p>
-
-                {showMenuElement1?  
-                <>   
+            <div className="menu-option foundation-menu">
+                <p className='menu-option-title'>The Foundation</p>
+                <div className='foundation-links'>   
                     <Link to='/about-us' className='menu-element'>About Kaffi</Link>
                     <Link to='/team' className='menu-element'>The Team</Link>
                     <Link to='/blog' className='menu-element'>Blog</Link>
                     <Link to='/records' className='menu-element'>Records & Success Stories</Link>
-                </>: null
-                }
+                </div>
             </div>
 
-            <div className="menu-option">
-                <p onClick={onClick2} className='menu-option-title'>Apply</p>
-
-                {showMenuElement2?  
-                <>
+            <div className="menu-option apply-menu">
+                <p className='menu-option-title'>Apply</p> 
+                <div className='apply-links'>
                     <Link to='/scholarships' className='menu-element'>Deadlines</Link>
                     <Link to='/scholarships/boost' className='menu-element'>Kaffi Boost</Link>
                     <Link to='/scholarships/launch' className='menu-element'>Kaffi Launch</Link>
-                </>: null
-                }
+                </div>
             </div>
 
-            <div className="menu-option">
-                <p onClick={onClick3} className='menu-option-title'>Donate</p>
-
-                {showMenuElement3?  
-                <>
+            <div className="menu-option donate-menu">
+                <p className='menu-option-title'>Donate</p>  
+                <div className='donate-links'>
                     <Link to='/donate' className='menu-element'>How To Donate</Link>
-                </>: null
-                }
+                </div>
             </div>
 
-            <div className="menu-option">
-                <p onClick={onClick4} className='menu-option-title'>Volunteer</p>
-
-                {showMenuElement4?  
-                <>                
+            <div className="menu-option volunteer-menu">
+                <p className='menu-option-title'>Volunteer</p>
+                <div className='volunteer-links'>                
                     <Link to='/volunteer/buddy' className='menu-element'>Become A Buddy</Link>
                     <Link to='/volunteer/team' className='menu-element'>Join The Team</Link>
-                </>: null
-                }
+                </div>
             </div>
 
-            <div className="menu-option">
-                <p onClick={onClick5} className='menu-option-title'>Academic Advising</p>
-
-                {showMenuElement5?  
-                <>                
+            <div className="menu-option academic-menu">
+                <p className='menu-option-title'>Academic Advising</p>
+                <div className='academic-links'>                
                     <Link to='/buddy/find' onhov className='menu-element'>Find A Buddy</Link>
                     <Link to='/cv' className='menu-element'>CV Tips</Link>
                     <Link to='/webinars' className='menu-element'>Webinars</Link>
-                </>: null
-                }
+                </div>
             </div>
         </div>
     </div>

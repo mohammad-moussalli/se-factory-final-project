6fd3c6e6925508866dc0aaf01f990c242b714839
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-27 17:27:15 +0300

    Add Accordion to Faq Component

--- front-end/reactjs/src/components/Faq.jsx
+++ front-end/reactjs/src/components/Faq.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState} from 'react'
 import axios from 'axios';
 import Spinner from './Spinner';
-import Button from "./Button";
-
+import '../style/faqs.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 const Faq = () => {
@@ -38,18 +42,31 @@ const Faq = () => {
        return <Spinner/>
     } else{
         return (
-            <div>
-                {FaqCategories.map((FaqCategory)=> {
-                return <button key={FaqCategory.id} onClick={() => getCategoryFaqs(FaqCategory.id)}>{FaqCategory.category}</button>
-                })}
+            <div className='faq-component'>
 
+                <div className='faq-component-header'>
+                    <p className='faq-component-title'>Frequently Asked Question</p>
+                    <p className='faq-component-subtitle'>Check our FAQ page for some common questions or email us at apply@kaffi-lb.org</p>
+                </div>
+
+                <div className='faq-component-category-buttons'>
+                    {FaqCategories.map((FaqCategory)=> {
+                    return <button className='faq-component-button' key={FaqCategory.id} onClick={() => getCategoryFaqs(FaqCategory.id)}>{FaqCategory.category}</button>
+                    })}
+                </div>
                 <div>
                     {Array.isArray(CategoryFaqs) && CategoryFaqs.map((CategoryFaq) => {
                         return(
-                            <div key={CategoryFaq.id}>
-                                {CategoryFaq.question}
-                                {CategoryFaq.answer}
-                            </div>  
+                            <div className='accordion'>
+                            <Accordion>
+                                <AccordionSummary className="accordion-summary" expandIcon={<ExpandMoreIcon />} aria-controls={`panel${CategoryFaq.id}a-content`} id={`panel${CategoryFaq.id}a-header`}>
+                                    <Typography className="faq-component-questions">{CategoryFaq.question}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails className="accordion-details">
+                                    <Typography className="faq-component-answers">{CategoryFaq.answer}</Typography>
+                                </AccordionDetails>
+                            </Accordion> 
+                            </div> 
                         )
                     })}
                 </div>

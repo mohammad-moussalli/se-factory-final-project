349e7f31ecb5154ef911e0396e55ec5be479da5d
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-29 23:58:51 +0300

    Change scholarship-application migration

---
+++ back-end/nodejs/migrations/20220329154014-create-scholarship-application.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Scholarship_Applications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      cycle_id: {
+        type: Sequelize.INTEGER
+      },
+      accepted: {
+        type: Sequelize.BOOLEAN
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      currency: {
+        type: Sequelize.STRING
+      },
+      degree: {
+        type: Sequelize.STRING
+      },
+      major: {
+        type: Sequelize.STRING
+      },
+      semester_tuition_fee: {
+        type: Sequelize.INTEGER
+      },
+      monthly_allowance: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Scholarship_Applications');
+  }
+};

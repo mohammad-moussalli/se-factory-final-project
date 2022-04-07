1b2227b896800b81daaa57be3538a4d83c570945
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-04-07 17:50:31 +0300

    Link Media Tags component with the Back-End

--- front-end/reactjs/src/pages/Blog.jsx
+++ front-end/reactjs/src/pages/Blog.jsx
@@ -1,23 +1,46 @@
 import MediaTag from "../components/MediaTag";
 import '../style/blog.css'
-import image1 from '../assets/images/Blog1.png';
-import image2 from '../assets/images/Blog2.png';
+import Spinner from '../components/Spinner';
+import { useEffect, useState} from 'react'
+import axios from 'axios';
 
 const Blog = () => {
-  return (
-    <div className="blog">
-      <div className="blog-header">
-          <p className="blog-title">Our Latest Blog</p>
-      </div>
-      <div className="blog-subheader">
-          <p className="blog-subtitle">Stay up to date with our latest news and sharings</p>
-      </div>
-      <div className="blog-media-tag">
-          <MediaTag link='https://www.elnashra.com/news/show/1513191' link_text='View Details' title='Featured at ElNashra' subtitle='Tuition in Western Europe is generally less of an issue compared to living expenses. The goal is to boost and support these students to remain in the country of study.' screenshot={image1}/>
-          <MediaTag link='https://www.instagram.com/p/CQdWQs0L_ga/?utm_source=ig_web_copy_link' link_text='View Details' title='Featured at Jusur Arabia' subtitle='Tuition in Western Europe is generally less of an issue compared to living expenses. The goal is to boost and support these students to remain in the country of study.' screenshot={image2}/>
-      </div>
-    </div>
-  )
+
+  const [blogs, setBlogs] = useState(null)
+  const blogApi = "http://localhost:8080/blogs/"
+
+  useEffect( () => {
+    axios.get(blogApi, {headers: {'Access-Control-Allow-Origin': '*'}})
+    .then(response => {
+        setBlogs(response.data)})
+    .catch(err => {
+        console.log(err)
+    });
+  }, [])
+
+  if (!blogs){
+       return <Spinner/>
+    } else{
+        return (
+          <div className="blog">
+            <div className="blog-header">
+                <p className="blog-title">Our Latest Blog</p>
+            </div>
+            <div className="blog-subheader">
+                <p className="blog-subtitle">Stay up to date with our latest news and sharings</p>
+            </div>
+            <div className="blog-media-tag">
+
+              {Array.isArray(blogs) && blogs.map((blog) => {
+                return(
+                <MediaTag link={blog.link} link_text={blog.text} title={blog.title} subtitle={blog.text} screenshot={blog.screenshot}/>
+                )
+              })}
+            </div>
+          </div>
+        )
+      }
 }
+  
 
 export default Blog

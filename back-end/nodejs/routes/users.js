d0ec960d1d96bdcbd3519641b13ae1e2422e56ba
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-04-07 03:14:17 +0300

    Add Routing for picture Upload

--- back-end/nodejs/routes/users.js
+++ back-end/nodejs/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var user = require("../controllers/users"); 
+const { cloudinary } = require('../config/cloudinary');
+
 
 /* GET users listing. */
 router.post('/register', user.register);
@@ -8,5 +10,13 @@ router.post('/login', user.login);
 router.get('/', user.getUser)
 router.get('/all', user.getAllUsers)
 router.post('/update', user.update)
-router.post('/image', user.uploadImage)
+// router.get("/image/:name", user.getImage);
+
+// router.get('/test1', user.getGoogleAuthURL)
+// router.post('/test2', user.getGoogleUser)
+router.post('/upload-image', user.updatePicture);
+
+
 module.exports = router;
+
+

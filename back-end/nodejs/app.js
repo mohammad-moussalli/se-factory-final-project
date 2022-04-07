dbebcbcb1e80d802b8b6da6df73c753643abc558
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-04-07 03:16:39 +0300

    Add app.use for cors and express

--- back-end/nodejs/app.js
+++ back-end/nodejs/app.js
@@ -18,10 +18,17 @@ var relationshipRouter = require('./routes/relationships');
 var app = express();
 app.use(fileupload())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+require('dotenv').config()
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use('/users', usersRouter);
 app.use('/scholarships', scholarshipRouter);

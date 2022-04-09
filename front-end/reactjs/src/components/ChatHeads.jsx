21fc3d3ee2601685999f0e82d004344b7cf0a8b9
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-04-09 22:56:02 +0300

    Impelement components for chatting

---
+++ front-end/reactjs/src/components/ChatHeads.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaEdit, FaEllipsisH, FaVideo } from "react-icons/fa";
+import "../style/chat-heads.css";
+
+const ChatHeads = ({ items, setReceiver }) => {
+
+  return (
+    <div>
+      <div className="conv-header-container">
+        <p className="conversations-header">Conversations</p>
+        <div>
+          <FaEllipsisH />
+          <FaVideo />
+          <FaEdit />
+        </div>
+      </div>
+      <input className="chat-heads-search" placeholder="Search in Messenger" />
+      <div className="chat-heads-container">
+        {items.map((obj, i) => (
+          <div
+            key={i}
+            className="chat-head-item"
+            onClick={() => setReceiver(obj)}
+          >
+            <div className="user-profile-pic-container">
+              <p className="user-profile-pic-text">{obj.email[0]}</p>
+            </div>
+            <p>{obj.email}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ChatHeads
